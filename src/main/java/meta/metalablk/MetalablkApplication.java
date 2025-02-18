11acdd82f1f98a839bb9c2bbb6dea9e9950f7d15
udara anuranga wanigasinghe <124368356+Anuranga200@users.noreply.github.com>
2025-02-18 12:53:31 +0530

    sprinboot project initialize

---
+++ src/main/java/meta/metalablk/MetalablkApplication.java
@@ -0,0 +1,13 @@
+package meta.metalablk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetalablkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetalablkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/meta/metalablk/MetalablkApplicationTests.java
@@ -0,0 +1,13 @@
+package meta.metalablk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetalablkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
