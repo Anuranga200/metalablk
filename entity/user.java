486b5a60551b562f3e2f63252892ff6897dfe741
udara anuranga wanigasinghe <124368356+Anuranga200@users.noreply.github.com>
2025-02-18 12:22:14 +0530

    cretae main files and coonect to database

---
+++ entity/user.java
@@ -0,0 +1,32 @@
+package META_LABLK.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;  // ADMIN or REPAIRMAN
+
+    @OneToMany(mappedBy = "assignedTo", cascade = CascadeType.ALL)
+    private Set<Task> tasks;
+}
